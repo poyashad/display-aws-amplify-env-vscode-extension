b61b32d41e123a44bce7854e163dec051a7c541d
Poja Shad <pojashad@Pojas-MacBook-Pro-3.local>
2022-05-06 12:29:51 +0200

    track local-env-info.json file

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# Display AWS Amplify Environment
+# Display Current AWS Amplify Environment
 
-This extension displays the AWS Amplify environment. It makes it easy for you to see which environment you are currently working in
+This extension displays the AWS Amplify environment. It makes it easy for you to see which environment you are currently working in.
+
+## Setup
+
+When you use the ```amplify checkout env``` command, Amplify updates the ```/amplify/.config/local-env-info.json``` file.
+
+The extension looks for changes in this file and displays the env name from this file. This file is located in .gitignore, which prevents the File System Watcher from finding the file and displaying the env name. You must have **Search: Use Ignore Files** disabled to enable this extension to work.
+
+![ignoreFiles](./img/search-use-ignore-files.png)
+
+## Use
+
+This extension displays the current Amplify Env name in the status bar of VS code. When you change the Amplify environment, this extension also notifies you with an informational message.
+
+![statusBar](./img/statusbar.png) ![message](./img/message.png)
+
--- src/extension.ts
+++ src/extension.ts
@@ -4,47 +4,9 @@ import * as vscode from "vscode";
 import { posix } from "path";
 let myStatusBarItem: vscode.StatusBarItem;
 
-export function activate(context: vscode.ExtensionContext) {
-  const myCommandId = "sample.showSelectionCount";
-  context.subscriptions.push(
-    vscode.commands.registerCommand(myCommandId, async () => {
-      if (!vscode.workspace.workspaceFolders) {
-		vscode.window.showInformationMessage(
-			"No folder or workspace opened"
-		  );
-      } else {
-		const folderUri = vscode.workspace.workspaceFolders[0].uri;
-		const fileUri = folderUri.with({
-		  path: posix.join(
-			folderUri.path,
-			"/amplify/.config",
-			"/local-env-info.json"
-		  ),
-		});
-		const readData = await vscode.workspace.fs.readFile(fileUri);
-		const readStr = Buffer.from(readData).toString("utf8");
-		const json = JSON.parse(readStr);
-		const env = json.envName;
-		vscode.window.showInformationMessage(
-		  `Your are working in Amplify Env: ${env}`
-		);
-	  }
-      
-    })
-  );
-  myStatusBarItem = vscode.window.createStatusBarItem(
-    vscode.StatusBarAlignment.Left,
-    100
-  );
-  myStatusBarItem.command = myCommandId;
-  context.subscriptions.push(myStatusBarItem);
-
-  updateStatusBarItem();
-}
-
-async function updateStatusBarItem(): Promise<void> {
+export async function activate(context: vscode.ExtensionContext) {
   if (!vscode.workspace.workspaceFolders) {
-    myStatusBarItem.hide();
+    vscode.window.showInformationMessage("No folder or workspace opened");
   } else {
     const folderUri = vscode.workspace.workspaceFolders[0].uri;
     const fileUri = folderUri.with({
@@ -54,13 +16,70 @@ async function updateStatusBarItem(): Promise<void> {
         "/local-env-info.json"
       ),
     });
-    const readData = await vscode.workspace.fs.readFile(fileUri);
-    const readStr = Buffer.from(readData).toString("utf8");
-    const json = JSON.parse(readStr);
-    myStatusBarItem.text = `$(split-vertical) Amplify Env: ${json.envName}`;
-    myStatusBarItem.show();
+
+    // init Status bar
+    const myCommandId = "sample.showSelectionCount";
+    myStatusBarItem = vscode.window.createStatusBarItem(
+      vscode.StatusBarAlignment.Left,
+      100
+    );
+    myStatusBarItem.command = myCommandId;
+    context.subscriptions.push(myStatusBarItem);
+    updateStatusBarItem(await getLocalEnv());
+
+    //Subscribes
+    context.subscriptions.push(
+      // När man klickar
+      vscode.commands.registerCommand(myCommandId, async () => {
+        if (!vscode.workspace.workspaceFolders) {
+          vscode.window.showInformationMessage("No folder or workspace opened");
+        } else {
+          vscode.window.showInformationMessage(
+            `Your are working in Amplify Environment: ${await getLocalEnv()}`
+          );
+        }
+      }),
+    );
+
+    context.subscriptions.push(
+      vscode.workspace.createFileSystemWatcher('**/local-env-info.json').onDidChange(async e => {
+        console.log(e);
+        if(e.path === fileUri.path){
+          vscode.window.showInformationMessage(
+            `Your are working in Amplify Environment: ${await getLocalEnv()}`
+          );
+          
+          updateStatusBarItem(await getLocalEnv());
+        }
+      })
+    );
   }
 }
+async function getLocalEnv(): Promise<String>{
+     if (!vscode.workspace.workspaceFolders) {
+      vscode.window.showInformationMessage("No folder or workspace opened");
+      return '';
+    } else {
+     const folderUri = vscode.workspace.workspaceFolders[0].uri;
+     const fileUri = folderUri.with({
+       path: posix.join(
+         folderUri.path,
+         "/amplify/.config",
+         "/local-env-info.json"
+       ),
+     });
+     const readData = await vscode.workspace.fs.readFile(fileUri);
+     const readStr = Buffer.from(readData).toString("utf8");
+     const json = JSON.parse(readStr);
+     const env = json.envName;
+     return env;
+    }
+}
+
+async function updateStatusBarItem(env: String): Promise<void> {
+    myStatusBarItem.text = `$(split-vertical) Amplify Environment: ${env}`;
+    myStatusBarItem.show();
+  }
 
 // this method is called when your extension is deactivated
 export function deactivate() {}
